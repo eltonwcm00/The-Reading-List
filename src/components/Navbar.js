7a03715a124e7a97d390834a1a5cb8759398fd1a
eltonwcm00 <eltonwcm00@gmail.com>
2021-07-08 23:07:55 +0800

    theme context - light & dark bg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
+
+    static contextType = ThemeContext; //specify and accessing the context type             
+                                       // * contextType can be usd in class based component only        
+
     render() {
+       
+        console.log(this.context);   
+        const { isLightTheme, light, dark } = this.context; //destructure the ThemeContext properties
+        const theme = isLightTheme ? light : dark; //light is used if isLightTheme is true
+
         return (
-           <nav>
+           <nav style={ {background: theme.ui, color: theme.syntax} }> {/* accessing theme properties */}
             <h1>Reading List</h1>
             <ul>
                 <li>Home</li>

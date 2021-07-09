7e645f38a650b6a5ebdb5848080c2b2b04ec785d
eltonwcm00 <eltonwcm00@gmail.com>
2021-07-09 22:23:19 +0800

    consume 2nd context

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,38 @@
 /* 2nd way of consuming the context *this method can be used in functional based component too */
 
 import React, { Component } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
 
 class Navbar extends Component {
 
     render() {
         return (
-            <ThemeContext.Consumer>
-                {(context) => {
-                
-                    const { isLightTheme, light, dark } = context; //destructure the ThemeContext properties
-                    const theme = isLightTheme ? light : dark; //light is used if isLightTheme is true
+            <AuthContext.Consumer>
+                {(authContext) => ( //2nd context return 1st context JSX
+                    <ThemeContext.Consumer>
+                        {(themeContext) => {
+                            const { isAuthenticated, toggleAuth } = authContext; //2nd context
+                            const { isLightTheme, light, dark } = themeContext; //destructure the ThemeContext properties
+                            const theme = isLightTheme ? light : dark; //light is used if isLightTheme is true
 
-                    return (
-                            <nav style={ {background: theme.ui, color: theme.syntax} }> {/* accessing theme properties */}
-                                <h1>Reading List</h1>
-                                <ul>
-                                    <li>Home</li>
-                                    <li>About</li>
-                                    <li>Contact</li>
-                                </ul>
-                            </nav> 
-                    );
-                }}
-            </ThemeContext.Consumer>
+                            return (
+                                <nav style={ {background: theme.ui, color: theme.syntax} }> {/* accessing theme properties */}
+                                    <h1>Reading List</h1>
+                                    <div onClick={toggleAuth}>
+                                        { isAuthenticated ? 'Logged in' : 'Logged out' }
+                                    </div>
+                                    <ul>
+                                        <li>Home</li>
+                                        <li>About</li>
+                                        <li>Contact</li>
+                                    </ul>
+                                </nav> 
+                            );
+                        }}
+                    </ThemeContext.Consumer>
+                )}
+            </AuthContext.Consumer>
         );
     }
 }

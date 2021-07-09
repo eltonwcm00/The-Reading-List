eb6f8800955949d78ab558c1b803910392ebe6fa
eltonwcm00 <eltonwcm00@gmail.com>
2021-07-09 23:57:23 +0800

    context api with functional based component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,27 +1,22 @@
-/* 1st way of consuming the context */
+/* 1st way of consuming the context (functional based component) */
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react'; //useContext is to enable context to be used in the functional based component
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
+const BookList = () => {
+    
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
 
-    static contextType = ThemeContext;
-
-    render() {
-
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
-
-        return (
-           <div className="book-list" style={ {color: theme.syntax, background: theme.bg} }>
-               <ul>
-                   <li style={ {background: theme.ui} }>The way of kings</li>
-                   <li style={ {background: theme.ui} }>The name of wind</li>
-                   <li style={ {background: theme.ui} }>The final empre</li>
-               </ul>
-           </div>
-        );
-    }
+    return (
+        <div className="book-list" style={ {color: theme.syntax, background: theme.bg} }>
+            <ul>
+                <li style={ {background: theme.ui} }>The way of kings</li>
+                <li style={ {background: theme.ui} }>The name of wind</li>
+                <li style={ {background: theme.ui} }>The final empre</li>
+            </ul>
+        </div>
+    );
 }
 
 export default BookList;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
-/* 2nd way of consuming the context *this method can be used in functional based component too */
+/* 2nd way of consuming the context *this method can be used in functional based component too (class based component)*/
 
 import React, { Component } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
-
+ 
 class Navbar extends Component {
 
     render() {
@@ -19,7 +19,7 @@ class Navbar extends Component {
                             return (
                                 <nav style={ {background: theme.ui, color: theme.syntax} }> {/* accessing theme properties */}
                                     <h1>Reading List</h1>
-                                    <div onClick={toggleAuth}>
+                                    <div onClick={ toggleAuth }>
                                         { isAuthenticated ? 'Logged in' : 'Logged out' }
                                     </div>
                                     <ul>

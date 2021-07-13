ad8cf37a3da598bc806a5e59e6f313d468dd84bb
eltonwcm00 <eltonwcm00@gmail.com>
2021-07-13 22:09:26 +0800

    multiple context with hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,28 @@
-/* 2nd way of consuming the context *this method can be used in functional based component too (class based component)*/
+/* OLD : 2nd way of consuming the context *this method can be used in functional based component too (class based component)*/
 
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { ThemeContext } from '../contexts/ThemeContext';
  
-class Navbar extends Component {
+const Navbar = () => {
 
-    render() {
-        return (
-            <AuthContext.Consumer>
-                {(authContext) => ( //2nd context return 1st context JSX
-                    <ThemeContext.Consumer>
-                        {(themeContext) => {
-                            const { isAuthenticated, toggleAuth } = authContext; //2nd context
-                            const { isLightTheme, light, dark } = themeContext; //destructure the ThemeContext properties
-                            const theme = isLightTheme ? light : dark; //light is used if isLightTheme is true
+    const { isLightTheme, light, dark } = useContext(ThemeContext); //destructure the ThemeContext properties
+    const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+    const theme = isLightTheme ? light : dark; //light is used if isLightTheme is true
 
-                            return (
-                                <nav style={ {background: theme.ui, color: theme.syntax} }> {/* accessing theme properties */}
-                                    <h1>Reading List</h1>
-                                    <div onClick={ toggleAuth }>
-                                        { isAuthenticated ? 'Logged in' : 'Logged out' }
-                                    </div>
-                                    <ul>
-                                        <li>Home</li>
-                                        <li>About</li>
-                                        <li>Contact</li>
-                                    </ul>
-                                </nav> 
-                            );
-                        }}
-                    </ThemeContext.Consumer>
-                )}
-            </AuthContext.Consumer>
-        );
-    }
+    return (
+        <nav style={ {background: theme.ui, color: theme.syntax} }> {/* accessing theme properties */}
+            <h1>Reading List</h1>
+            <div onClick={ toggleAuth }>
+                { isAuthenticated ? 'Logged in' : 'Logged out' }
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav> 
+    );
 }
 
 export default Navbar;

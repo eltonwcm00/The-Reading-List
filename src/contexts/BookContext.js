038ba0fe0fa85c88181bf7a58fb7acfde8411416
eltonwcm00 <eltonwcm00@gmail.com>
2021-07-16 22:45:37 +0800

    implement localStorage

--- src/contexts/BookContext.js
+++ src/contexts/BookContext.js
@@ -1,6 +1,6 @@
 // Fucntional based componenet context
 
-import React, { createContext, useState, useReducer } from 'react';
+import React, { createContext, useState, useReducer, useEffect } from 'react';
 import uuid from 'uuid/v1';
 import { bookReducer } from '../reducers/bookReducer';
 
@@ -10,7 +10,22 @@ const BookContextProvider = (props) => {
     
     // dispatch is the useReducer default function
     // useReducer(name of the reducer, data)
-    const [books, dispatch] = useReducer(bookReducer, []);
+    // 3rd argument is optional, to get data from local storage
+    const [books, dispatch] = useReducer(bookReducer, [], () => {
+        const localData = localStorage.getItem('books');
+
+        if (localData) {
+            return JSON.parse(localData);
+        } else {
+            return [];
+        }
+    });
+
+    // whenever any changes in 'books', run this hook
+    useEffect(() => {
+        // setItem(key, value), stringify turn object into string
+        localStorage.setItem('books', JSON.stringify(books));
+    }, [books]);
 
     return(
         <BookContext.Provider value={ {books, dispatch} }>
